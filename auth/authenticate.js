e010a3afeebf62a625b170214b2e4e77f18bfff8
Sascha Majewsky <sascha.majewsky@pm.me>
2019-07-23 11:23:36 +0200

    finished mvp for the login, register and jokes route

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -7,6 +7,7 @@ const jwtKey =
 // quickly see what this file exports
 module.exports = {
   authenticate,
+  generateJWT,
 };
 
 // implementation details
@@ -26,4 +27,15 @@ function authenticate(req, res, next) {
       error: 'No token provided, must be set on the Authorization Header',
     });
   }
+}
+
+function generateJWT(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+  const options = {
+    expiresIn: '1h',
+  };
+  return jwt.sign(payload, jwtKey, options);
 }
